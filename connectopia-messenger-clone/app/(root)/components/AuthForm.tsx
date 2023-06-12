d1401ba5be91d56eea7a313871878d78a035baac
subham07-t <82716446+subham07-t@users.noreply.github.com>
2023-06-13 00:19:44 +0530

    feat: Authform variant and react-hook-form setup

--- connectopia-messenger-clone/app/(root)/components/AuthForm.tsx
+++ connectopia-messenger-clone/app/(root)/components/AuthForm.tsx
@@ -1,5 +1,50 @@
+"use client";
+
+import { useCallback, useState } from "react";
+import { useForm, FieldValues, SubmitHandler } from "react-hook-form";
+
+type variant = "LOGIN" | "REGISTER";
 const AuthForm = () => {
-  return <div>AuthForm</div>;
+  const [variant, setVariant] = useState<variant>("LOGIN");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const toggleVariant = useCallback(() => {
+    if (variant === "LOGIN") {
+      setVariant("REGISTER");
+    } else {
+      setVariant("LOGIN");
+    }
+  }, [variant]);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+    if (variant === "REGISTER") {
+    }
+    if (variant === "LOGIN") {
+    }
+  };
+
+  const socialAction = (action: string) => {
+    setIsLoading(true);
+  };
+
+  return (
+    <div className="">
+      <div></div>
+    </div>
+  );
 };
 
 export default AuthForm;
