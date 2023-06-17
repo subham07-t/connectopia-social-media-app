deb23778fe37e86f763d7992f33acc2754dd09fd
subham07-t <82716446+subham07-t@users.noreply.github.com>
2023-06-17 17:10:40 +0530

    feat: sidebar,mobilefooter created

--- connectopia-messenger-clone/app/users/layout.tsx
+++ connectopia-messenger-clone/app/users/layout.tsx
@@ -1,7 +1,15 @@
+import Sidebar from "../components/sidebar/Sidebar";
+
 export default async function UsersLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <div className="h-full">{children}</div>;
+  return (
+    <>
+      <Sidebar>
+        <div className="h-full">{children}</div>;
+      </Sidebar>
+    </>
+  );
 }
